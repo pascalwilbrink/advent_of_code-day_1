0345a4f9f8f726ec95864259e45992030a1f7813
Pascal Wilbrink <pascal.wilbrink@alliander.com>
2022-12-01 20:11:53 +0100

    Added part 2

--- src/part_2.ts
+++ src/part_2.ts
@@ -1,12 +1,15 @@
 import { input } from "./input";
 
-const res: number = Math.max(
-  ...input.split("\n\n").map((res) => {
+const res: number = input
+  .split("\n\n")
+  .map((res) => {
     return res
       .split("\n")
       .map((val) => Number(val))
       .reduce((a, b) => a + b);
   })
-);
+  .sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((a, b) => a + b);
 
 console.log(res);
